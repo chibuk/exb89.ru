ea6c83bb1b1f434484d7c9908e087c7d4c2e17ac
Юрий <Jouri@localhost.localdomain>
2025-02-24 17:46:47 +0500

    +Администрирование списков через админку django

--- shoplist/admin.py
+++ shoplist/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ListItem
 
 # Register your models here.
+# admin.site.register(ListItem)
+
+class ListItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'list_name', 'isdone', 'created', 'modified',)  # Поля для отображения в списке
+    search_fields = ('name', 'list_name',)  # Поля для поиска
+
+admin.site.register(ListItem, ListItemAdmin)
+
